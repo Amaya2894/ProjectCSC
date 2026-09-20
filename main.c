2ff064b0c6885c33d402df5814780529dbaf8a0d
Amaya2894 <jananiamaya2025.gmail.com>
2026-09-20 10:51:30 +0530

    Added ward data and capacity details

--- main.c
+++ main.c
@@ -10,6 +10,11 @@ const float BASE_FEES[4] = {1500.00, 2500.00, 4500.00, 5000.00};
 const int CONSULT_TIMES[4] = {15, 20, 30, 30};
 int queueCounts[4] = {0, 0, 0, 0};
 
+// Ward Data
+const char WARD_NAMES[4][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
+const float WARD_RATES[4] = {3000.00, 6000.00, 12000.00, 25000.00};
+const int WARD_CAPACITIES[4] = {20, 10, 10, 5};
+
 int main()
 {
     printf("Smart Hospital Management System...\n");
